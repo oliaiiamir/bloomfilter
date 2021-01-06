0fd74e7b0be5d19ffd48af260bbc85f7bd309327
oliaiiamir <52468349+oliaiiamir@users.noreply.github.com>
2021-01-06 16:15:29 +0330

    Update getFalsePositiveProbability.m

--- getFalsePositiveProbability.m
+++ getFalsePositiveProbability.m
@@ -1,24 +1,21 @@
-###############################################################################
-# FUNCTION getFalsePositiveProbability (getFalsePositiveProbability.m)
-# --> to work with BloomFilter function (BloomFilter.m)
-###############################################################################
-# Authors:
-# ----> Cristiano Vagos (NMec 65169) 
-# ----> Ariel Bastos    (NMec 72204)
-#
-# Calculate the Probability of False Positives of a given Bloom Filter.
-# Formula: (1 - e^(-k * n / m)) ^ k
-#
-# Inputs:
-# ----> bf:  a Bloom Filter previously created
-#
-# Output:
-# ----> value: value of the probability
-#
-# Usage:
-# ----> probability = getFalsePositiveProbability(filter);
-# 
+%#####################################################
+%  BloomFilter (BloomFilter.m)
+%
+%  Forked by: https://github.com/oliaiiamir
+%
+%  Calculate the Probability of False Positives of a given Bloom Filter.
+%  Formula: (1 - e^(-k * n / m)) ^ k
+% 
+%  Inputs:
+%  ----> bf:  a Bloom Filter previously created
+% 
+%  Output:
+%  ----> value: value of the probability
+% 
+%  Usage:
+%  ----> probability = getFalsePositiveProbability(filter);
+%#####################################################
 
 function value = getFalsePositiveProbability(bf)
-  value = ((1 - exp(-bf.k*bf.elementsAdded / bf.size)) ^ bf.k)
-endfunction
+  value = ((1 - exp(-bf.k*bf.elementsAdded / bf.size)) ^ bf.k);
+end
